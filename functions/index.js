1d6ea1c16ee614fc11b0fed613a8d03968d1861a
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-09-21 11:52:07 -0500

    start using express, also make sure newest screams are returned first

--- functions/index.js
+++ functions/index.js
@@ -2,24 +2,38 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 admin.initializeApp();
+const express = require('express');
+const app = express();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 
-exports.getScreams = functions.https.onRequest((request, response) => {
-    admin.firestore().collection('screams').get()
+app.get('/screams', (request, response) => {
+    admin
+        .firestore()
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
         .then(data => {
             let screams = [];
             data.forEach(doc => {
-                screams.push(doc.data());
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
             });
             return response.json(screams)
         })
         .catch((err) => console.error(err))
 });
 
-exports.createScream = functions.https.onRequest((request, response) => {
+app.post('/scream', (request, response) => {
+    if (request.method !== 'POST'){
+        return response.status(400).json({ error: "Method not allowed"})
+    }
     const newScream = {
         body: request.body.body,
         userHandle: request.body.userHandle,
@@ -38,3 +52,4 @@ exports.createScream = functions.https.onRequest((request, response) => {
         });
 });
 
+exports.api = functions.https.onRequest(app);
