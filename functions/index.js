c40533e281529cfb653c4fff0cec77acd322126b
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-09-30 14:42:19 -0500

    add like and unlike functionality for screams

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,38 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const app = require('express')();
-const FBAuth = require('./util/fbAuth');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const app = require("express")();
+const FBAuth = require("./util/fbAuth");
 
-const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+  likeScream,
+  unlikeScream
+} = require("./handlers/screams");
 
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser
+} = require("./handlers/users");
 
 //scream routes
-app.get('/screams', getAllScreams);
-app.get('/screams/:screamId', getScream);
-app.post('/scream', FBAuth, postOneScream);
-app.post('/screams/:screamId/comment', FBAuth, commentOnScream);
+app.get("/screams", getAllScreams);
+app.get("/screams/:screamId", getScream);
+app.post("/scream", FBAuth, postOneScream);
+app.post("/screams/:screamId/comment", FBAuth, commentOnScream);
+app.get("/screams/:screamId/like", FBAuth, likeScream);
+app.get("/screams/:screamId/unlike", FBAuth, unlikeScream);
 
-// users routes 
-app.post('/signup', signup);
-app.post('/login', login);
-app.post('/user/image', FBAuth, uploadImage);
-app.post('/user', FBAuth, addUserDetails);
-app.get('/user', FBAuth, getAuthenticatedUser);
+// users routes
+app.post("/signup", signup);
+app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
